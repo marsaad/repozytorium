43a450f3dce987fa426856d5e6794eb5b79c09d6
marsaad <marsad7@o2.pl>
2020-03-16 23:42:37 +0100

    hello world + b

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-    int a;
-    a=5;
+    int b;
+    b=15;
     std::cout << "Hello, World!" << std::endl;
-    std::cout << a << std::endl;
+    std::cout << b << std::endl;
     return 0;
 }
