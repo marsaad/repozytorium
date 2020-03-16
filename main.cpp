52f3f039d360cf6820000e12286d6d24ba23286b
marsaad <marsad7@o2.pl>
2020-03-17 00:18:51 +0100

    zmiana na dodawanie trzech liczb

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 int main() {
     int pierwsza_liczba;
     int druga_liczba;
+    int trzecia_liczba;
     std::cout << "Podaj pierwszą liczbę" << std::endl;
     std::cin >> pierwsza_liczba;
     std::cout << "Podaj drugą liczbę" << std::endl;
     std::cin >> druga_liczba;
-    int wynik = pierwsza_liczba + druga_liczba;
+    std::cout << "Podaj trzecia liczbę" << std::endl;
+    std::cin >> trzecia_liczba;
+    int wynik = pierwsza_liczba + druga_liczba + trzecia_liczba;
     std::cout << wynik << std::endl;
     return 0;
 }
